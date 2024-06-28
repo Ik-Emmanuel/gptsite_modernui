43702c4fefd89dcc53e6b79c1d58f010dd63fb1c
Ik-Emmanuel <nwokochaikechukwu13@gmail.com>
2024-06-28 20:58:48 +0100

    header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,60 @@
+import Image from "next/image";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div
+      className="flex flex-col lg:flex-row px-[2rem] lg:px-[4rem] py-[2rem] mt-4"
+      id="home"
+    >
+      <div className="flex flex-[0.5] justify-center items-start flex-col mr-[5rem]">
+        <h1 className="font-[800] text-[32px] lg:text-[62px] leading-[35px] lg:leading-[75px] tracking-[-0.04em] gradient__text">
+          Let&apos;s Build Something amazing with GPT-3 OpenAI
+        </h1>
+
+        <p className=" text-[#81AFDD] font-[400] text-[12px] lg:text-[20px] leading-[28px mt-[1.5rem]">
+          This is an open-source project created by The Open AI. It can be used
+          to generate text and images. and it is the best GPT-3 OpenAI.To begin,
+          simply sign-up using the link below.
+        </p>
+
+        <div className="w-full mx-[0rem] mr-0 my-[1rem] flex ">
+          <input
+            className="flex flex-[2] min-h-[50px] font-[400] text-[12px] lg:text-[20px] leading-[28px] text-white bg-[#052D45] rounded-[5px] px-2 lg:px-5 py-2 lg:py-4 border-[2px] border-[#031B34] outline-none rounded-tl-[10px] rounded-bl-[10px]"
+            type="email"
+            placeholder="Enter Your Email Address"
+          />
+          <button
+            className="flex flex-[0.6] w-full min-h-[50px] font-[400] text-[12px] lg:text-[20px] leading-[28px] text-white bg-[#FF4820] rounded-[5px] px-5 py-4 border-[2px] border-[#FF4820] outline-none rounded-tr-[10px] rounded-br-[10px]"
+            type="button"
+          >
+            Get Started
+          </button>
+        </div>
+
+        <div className="w-full flex flex-col lg:flex-row justify-start items-center mt-[2rem]">
+          <Image
+            src={"/assets/people.png"}
+            width={182}
+            height={38}
+            alt="people_image"
+          />
+          <p className="mt-0 mr-0  mb-0  ml-[1rem] font-[500] text-[12px] leading-[38px] text-[#fff] text-center">
+            1,600 people requested access a visit in last 24 hours
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-[0.7] justify-center items-center">
+        <Image
+          src={"/assets/ai.png"}
+          width={800}
+          height={800}
+          quality={100}
+          alt="ai_image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
